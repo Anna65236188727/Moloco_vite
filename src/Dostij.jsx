88def75b1ca5214d4e836639853d5ad6e6a626cb
Anna65236188727 <94472429+Anna65236188727@users.noreply.github.com>
2023-10-22 13:07:12 +0300

    Create Dostij.jsx

---
+++ src/Dostij.jsx
@@ -0,0 +1,14 @@
+import image15 from "./assets/image15.png";
+import image16 from "./assets/image16.png";
+import image17 from "./assets/image17.png";
+
+function Dostij() {
+    return <>
+        <div className="dostij">
+            <div className="text3">ДОСТИЖЕНИЯ И НАГРАДЫ</div>
+            <div className="text3">Продукция с Российским знаком качества!</div>
+        </div>
+    </>
+}
+
+export default Dostij;
